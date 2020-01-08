38f0e5f9ead7930bdb10bfadcf5fba955e489883
Dhawale, Dinkar <dinkar.dhawale@intel.com>
2020-01-08 20:41:19 +0530

    update release version

--- change-maven-version.sh
+++ change-maven-version.sh
@@ -29,18 +29,17 @@ changeVersionCommand="mvn versions:set -DnewVersion=${version}"
 changeParentVersionCommand="mvn versions:update-parent -DallowSnapshots=true -DparentVersion=${version}"
 mvnInstallCommand="mvn clean install"
 
-(cd tbootxm-maven-root && $changeVersionCommand)
-if [ $? -ne 0 ]; then echo "Failed to change maven version on \"tbootxm-maven-root\" folder" >&2; exit 3; fi
-ant ready
-if [ $? -ne 0 ]; then echo "Failed to run \"ant ready\" command" >&2; exit 3; fi
-$changeVersionCommand
-if [ $? -ne 0 ]; then echo "Failed to change maven version at top level" >&2; exit 3; fi
-$changeParentVersionCommand
-if [ $? -ne 0 ]; then echo "Failed to change maven parent versions" >&2; exit 3; fi
-
 (cd packages  && $changeVersionCommand)
 if [ $? -ne 0 ]; then echo "Failed to change maven version on \"packages\" folder" >&2; exit 3; fi
 (cd packages  && $changeParentVersionCommand)
+if [ $? -ne 0 ]; then echo "Failed to change maven parent versions in \"packages\" folder" >&2; exit 3; fi
+(cd packages/application-agent && $changeVersionCommand)
+if [ $? -ne 0 ]; then echo "Failed to change maven version on \"packages\" folder" >&2; exit 3; fi
+(cd packages/application-agent && $changeParentVersionCommand)
+if [ $? -ne 0 ]; then echo "Failed to change maven parent versions in \"packages\" folder" >&2; exit 3; fi
+(cd packages/tbootxm-zip && $changeVersionCommand)
+if [ $? -ne 0 ]; then echo "Failed to change maven version on \"packages\" folder" >&2; exit 3; fi
+(cd packages/tbootxm-zip && $changeParentVersionCommand)
 if [ $? -ne 0 ]; then echo "Failed to change maven parent versions in \"packages\" folder" >&2; exit 3; fi
 sed -i 's/\-[0-9\.]*\(\-SNAPSHOT\|\(\-\|\.zip$\|\.bin$\|\.jar$\)\)/-'${version}'\2/g' build.targets
 if [ $? -ne 0 ]; then echo "Failed to change versions in \"build.targets\" file" >&2; exit 3; fi
